d72a9fff228a5bb1509199e9c17935fe941d1ec9
Mahmoud Adly <MahmoudAdly@users.noreply.github.com>
2022-12-11 11:24:32 +0100

    Fix config script

--- configuration_maker.py
+++ configuration_maker.py
@@ -9,6 +9,14 @@ config_object = None
 reader = SimpleMFRC522()
 
 
+def update_config():
+  # Write the new config object to the config file
+  json_object = json.dumps(config_object, indent=2)
+  with open(CONFIG_FILE_NAME, "w") as outfile:
+    outfile.write(json_object)
+  print(config_object)
+
+
 # Fetch the configs from a file
 with open(CONFIG_FILE_NAME, 'r') as openfile:
   # Reading from json file
@@ -24,6 +32,9 @@ while True:
     description = input("Please enter a short description to remember this ID, or 'Q' to exit: ")
 
     if description == "Q":
+      update_config()
+      print("Config update is done! Goodbye.")
+      exit(0)
       break
 
     # Make sure the config key has an initialised empty dict value if it was new
@@ -35,12 +46,5 @@ while True:
     print("Sorry, I didn't understand that.")
     # Better try again... Return to the start of the loop
     continue
-
-print(config_object)
-
-# Write the new config object to the config file
-json_object = json.dumps(config_object, indent=2)
-with open(CONFIG_FILE_NAME, "w") as outfile:
-  outfile.write(json_object)
- 
-print("Config update is done! Goodbye.")
+  finally:
+    GPIO.cleanup()
